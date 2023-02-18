cf4d6458f95d5afb384d7b3fab27fea1f65a3ba2
Kenya Nonaka <kenya.nonaka@uzabase.com>
2023-02-18 05:45:19 +0000

    add scipy

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.0.1", 
     description="caluculate association about nominal variables in pandas framework",
     author='nonaka',
-    packages=find_packages(),  # 使うモジュール一覧を指定する
+    packages=find_packages(),
     license='MIT',
-    install_requires=["pandas==1.5.3"],
+    install_requires=["pandas==1.5.3", "scipy==1.10.0"],
     extra_require={
         "dev": ["pytest", "black", "isort"]
     }
