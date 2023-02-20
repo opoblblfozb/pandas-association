2e3cb581bccfbe00687ce2e242f51b4158f70484
Kenya Nonaka <kenya.nonaka@uzabase.com>
2023-02-20 12:30:24 +0000

    modify dependencies version

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='pandas-association',  
     version="0.0.1", 
+    python_requires=">=3.10.0",
     description="caluculate association about nominal variables in pandas framework",
     author='nonaka',
     packages=find_packages(),
     license='MIT',
-    install_requires=["pandas==1.5.3", "scipy==1.10.0", "scikit-learn==1.2.1", "pytest"],
+    install_requires=["pandas>=1.5.3", "scipy>=1.10.0", "scikit-learn>=1.2.1", "pytest"],
 )
